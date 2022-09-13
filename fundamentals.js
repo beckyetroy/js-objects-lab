a312a98c73f9df531c6be6e405eba030164fd155
Rebecca Troy <20088744@mail.wit.ie>
2022-09-13 11:10:21 +0100

    dynamic properties
    This commit completes the dynamic properties
    section of the lab.
    
    Signed-off-by: Rebecca Troy <20088744@mail.wit.ie>

--- fundamentals.js
+++ fundamentals.js
@@ -16,6 +16,14 @@ const aCar = {
         year: 201,
         county: 'WD',
         number: '1058',
+    },
+    mileage: 1000,
+    colour: {
+        exterior: 'red',
+        interior: {
+            texture: 'leather',
+            shade: 'cream'
+        }
     }
 };
 
@@ -25,4 +33,7 @@ console.log('Reg. = ' + aCar.registration.year + '-' +
     aCar.registration.county + '-' + aCar.registration.number + '.');
 
 console.log('First owner : ' + aCar.previous_owners[0].name + ' - '
-    + aCar.previous_owners[0].address); 
+    + aCar.previous_owners[0].address);
+
+console.log('It is a ' + aCar.colour.exterior + ' car, ' + aCar.mileage
+    + ' mileage, with ' + aCar.colour.interior.texture + ' interior.');
